752762aaf4630995b12d6dce2761b2b2b7647bf9
Aleksei Struchev <astruchev@gmail.com>
2022-12-30 22:12:43 +0300

    added test_guest_should_see_login_link method

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,18 @@
 from selenium.webdriver import Remote as RemoteWebDriver
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
-    def __init__(self, browser: RemoteWebDriver, url):
+    def __init__(self, browser: RemoteWebDriver, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how_to_find, what):
+        try:
+            self.browser.find_element(how_to_find, what)
+        except NoSuchElementException:
+            return False
+        return True
 
     def open(self):
         self.browser.get(self.url)
