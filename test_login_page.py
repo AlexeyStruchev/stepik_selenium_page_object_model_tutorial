8226b5d40c61121c7e4d4d25441884267e95cbc3
Aleksei Struchev <astruchev@gmail.com>
2023-01-01 14:29:54 +0300

    Fixed test_login_page.py tests. Tests work now.

--- test_login_page.py
+++ test_login_page.py
@@ -1,9 +1,26 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
-
-def test_guest_should_be_login_form(browser):
+def test_should_be_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = MainPage(browser, link, 5)
+    page = LoginPage(browser, link, 5)
     page.open()
     page.should_be_login_form()
+
+def test_should_be_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link, 5)
+    page.open()
+    page.should_be_register_form()
+
+def test_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link, 5)
+    page.open()
+    page.should_be_login_url()
+
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link, 5)
+    page.open()
+    page.should_be_login_page()
