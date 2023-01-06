9f4cd84322c2b2d81d0e66ee5fb100a209c7751a
Aleksei Struchev <astruchev@gmail.com>
2023-01-06 12:34:01 +0300

    Created equality test for an url parameter presence in product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,20 +4,24 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def basket_popup_message_and_price_should_be_equal_to_title_message_and_price(self):
-        self.should_be_promo_parameter_in_url("promo=newYear2019")
+        self.should_be_promo_parameter_in_url(self.get_parameter_from_url())
         self.click_add_to_basket_button()
         self.solve_quiz_and_get_code()
-        product_title = self.get_product_title()
-        self.product_title_equal_to_basket_popup_title(product_title)
-        product_price = self.get_product_price()
-        self.product_price_is_equal_to_basket_popup_price(product_price)
+        self.product_title_equal_to_basket_popup_title(self.get_product_title())
+        self.product_price_is_equal_to_basket_popup_price(self.get_product_price())
 
     def get_parameter_from_url(self):
-        # to be done
-        pass
+        separated_list = self.browser.current_url.split('?')
+        return str(separated_list[1])
+
+    def should_be_login_url(self):
+        assert self.browser.current_url.find("login"), "login word is present in url"
+
 
     def should_be_promo_parameter_in_url(self, url_parameter_):
-        assert self.browser.current_url.find(url_parameter_), f"Parameter < {url_parameter_} >  is present in url"
+        check_parameter = "promo=newYear2019"
+        assert url_parameter_ == check_parameter,\
+            f"Parameter < {url_parameter_} > has to be equal to < {check_parameter} >"
 
     def click_add_to_basket_button(self):
         add_to_basket_button = self.is_element_present(*ProductPageLocators.product_add_to_basket_button)
