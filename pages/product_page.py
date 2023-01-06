03d1fe5b26d7a99c37e1d9a1f81d3ce64644f904
Aleksei Struchev <astruchev@gmail.com>
2023-01-06 16:49:25 +0300

    technical commit

--- pages/product_page.py
+++ pages/product_page.py
@@ -23,21 +23,39 @@ class ProductPage(BasePage):
             f"Parameter < {url_parameter_} > has to be equal to < {check_parameter} >"
 
     def click_add_to_basket_button(self):
-        add_to_basket_button = self.is_element_present(*ProductPageLocators.product_add_to_basket_button)
+        add_to_basket_button = self.is_element_present(*ProductPageLocators.PRODUCT_ADD_TO_BASKET_BUTTON)
         add_to_basket_button.click()
 
     def get_product_title(self):
-        return self.is_element_present(*ProductPageLocators.product_title).text
+        return self.is_element_present(*ProductPageLocators.PRODUCT_TITLE).text
 
     def get_product_price(self):
-        return self.is_element_present(*ProductPageLocators.product_price).text
+        return self.is_element_present(*ProductPageLocators.PRODUCT_PRICE).text
 
     def product_title_equal_to_basket_popup_title(self, product_title_):
-        basket_popup_title_text = self.is_element_present(*ProductPageLocators.product_basket_popup_title).text
+        basket_popup_title_text = self.is_element_present(*ProductPageLocators.PRODUCT_BASKET_POPUP_TITLE).text
         assert product_title_  == basket_popup_title_text,\
             f'A basket popup title < {basket_popup_title_text} > has to be equal to a product title < {product_title_} >'
 
     def product_price_is_equal_to_basket_popup_price(self, product_price_):
-        basket_popup_price_text = self.is_element_present(*ProductPageLocators.product_basket_popup_price).text
+        basket_popup_price_text = self.is_element_present(*ProductPageLocators.PRODUCT_BASKET_POPUP_PRICE).text
         assert product_price_  == basket_popup_price_text, \
             f'A basket popup price < {basket_popup_price_text} > has to be equal to a product price < {product_price_} >'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_element_disapper(self):
+        assert self.is_disappeared(*ProductPageLocators.DISAPPERED_ELEMENT), \
+            "Success message is presented, but should not be"
+
+    def basket_cant_see_success_message_after_adding_product_to_basket(self):
+        self.click_add_to_basket_button()
+        self.should_not_be_success_message()
+
+    def basket_guest_cant_see_success_message(self):
+        self.should_not_be_success_message()
+
+    def basket_delivery_message_disappeared_after_adding_product_to_basket(self):
+        self.should_element_disapper()
