4af36f03013f88e8f697c986676f8be5787c538f
Aleksei Struchev <astruchev@gmail.com>
2023-01-11 12:57:23 +0300

    Second myself review for final task: sorted methods by alphabet

--- pages/locators.py
+++ pages/locators.py
@@ -11,24 +11,24 @@ class BasePageLocators():
     USER_ICON = (By.CLASS_NAME, "icon-user")
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.ID, "login_form")
     LOGIN_EMAIL = (By.ID, "id_login-username")
+    LOGIN_FORM = (By.ID, "login_form")
     LOGIN_PASSWORD = (By.ID, "id_login-password")
-    REGISTER_FORM = (By.ID, "register_form")
-    REGISTER_EMAIL = (By.ID, "id_registration-email")
-    REGISTER_PASSWORD = (By.ID, "id_registration-password1")
-    REGISTER_CONFIRM_PASSWORD = (By.ID, "id_registration-password2")
     REGISTER_BUTTON = (By.CSS_SELECTOR, '[name="registration_submit"]')
+    REGISTER_CONFIRM_PASSWORD = (By.ID, "id_registration-password2")
+    REGISTER_EMAIL = (By.ID, "id_registration-email")
+    REGISTER_FORM = (By.ID, "register_form")
+    REGISTER_PASSWORD = (By.ID, "id_registration-password1")
 
 class ProductPageLocators():
+    DISAPPERED_ELEMENT = (By.CSS_SELECTOR, '#messages > div:nth-child(2) > div')
     PRODUCT_ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, 'btn-add-to-basket')
-    PRODUCT_TITLE = (By.CSS_SELECTOR, '.product_main > h1')
     PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main > .price_color')
-    PRODUCT_BASKET_POPUP_TITLE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
+    PRODUCT_TITLE = (By.CSS_SELECTOR, '.product_main > h1')
     PRODUCT_BASKET_POPUP_PRICE = (By.CSS_SELECTOR, '#messages > div:nth-child(3) > div > p > strong')
+    PRODUCT_BASKET_POPUP_TITLE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div')
-    DISAPPERED_ELEMENT  = (By.CSS_SELECTOR, '#messages > div:nth-child(2) > div')
 
 class BasketPageLocators():
-    BASKET_IS_EMPTY_TEXT = (By.ID, 'content_inner')
     BASKET_IS_EMPTY = (By.CLASS_NAME, 'col-sm-6')
+    BASKET_IS_EMPTY_TEXT = (By.ID, 'content_inner')
--- pages/login_page.py
+++ pages/login_page.py
@@ -3,24 +3,10 @@ from pages.base_page import BasePage
 from pages.locators import LoginPageLocators
 from faker import Faker
 
+
 class LoginPage(BasePage):
-    def register_new_user(self):
-        self.fill_email()
-        self.fill_password()
-        self.click_register_button()
-    def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
-
-    def should_be_login_url(self):
-        assert self.browser.current_url.find("login"), "login word is present in url"
-
-    def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is present"
-
-    def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is present"
+    def click_register_button(self):
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
 
     def fill_email(self):
         fake = Faker()
@@ -32,5 +18,21 @@ class LoginPage(BasePage):
         self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD).send_keys(password)
         self.browser.find_element(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD).send_keys(password)
 
-    def click_register_button(self):
-        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+    def register_new_user(self):
+        self.fill_email()
+        self.fill_password()
+        self.click_register_button()
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is present"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is present"
+
+    def should_be_login_url(self):
+        assert self.browser.current_url.find("login"), "login word is present in url"
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
