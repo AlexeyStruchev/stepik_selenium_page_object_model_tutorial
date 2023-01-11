e0a58245996d94b481e0125a5c8dcef667f3b037
Aleksei Struchev <astruchev@gmail.com>
2023-01-11 15:24:15 +0300

    Third myself review for final task: sorted methods by alphabet

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,28 +3,22 @@ from pages.locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
+    def add_to_basket(self):
+        self.click_add_to_basket_button()
+
     def basket_popup_message_and_price_should_be_equal_to_title_message_and_price(self):
-        # self.should_be_promo_parameter_in_url(self.get_parameter_from_url(), "promo=newYear2019")
         self.click_add_to_basket_button()
         self.solve_quiz_and_get_code()
         self.product_title_equal_to_basket_popup_title(self.get_product_title())
         self.product_price_is_equal_to_basket_popup_price(self.get_product_price())
 
-    def get_parameter_from_url(self):
-        separated_list = self.browser.current_url.split('?')
-        return str(separated_list[1])
-
-    def should_be_login_url(self):
-        assert self.browser.current_url.find("login"), "login word is present in url"
-
-
-    def should_be_promo_parameter_in_url(self, url_parameter_, check_parameter):
-        assert url_parameter_ == check_parameter,\
-            f"Parameter < {url_parameter_} > has to be equal to < {check_parameter} >"
-
     def click_add_to_basket_button(self):
         add_to_basket_button = self.is_element_present(*ProductPageLocators.PRODUCT_ADD_TO_BASKET_BUTTON)
         add_to_basket_button.click()
+
+    def get_parameter_from_url(self):
+        separated_list = self.browser.current_url.split('?')
+        return str(separated_list[1])
 
     def get_product_title(self):
         return self.is_element_present(*ProductPageLocators.PRODUCT_TITLE).text
@@ -42,13 +36,17 @@ class ProductPage(BasePage):
         assert product_price_  == basket_popup_price_text, \
             f'A basket popup price < {basket_popup_price_text} > has to be equal to a product price < {product_price_} >'
 
-    def should_not_be_success_message(self):
-        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
-            "Success message is presented, but should not be"
+    def should_be_login_url(self):
+        assert self.browser.current_url.find("login"), "login word is present in url"
+
+    def should_be_promo_parameter_in_url(self, url_parameter_, check_parameter):
+        assert url_parameter_ == check_parameter,\
+            f"Parameter < {url_parameter_} > has to be equal to < {check_parameter} >"
 
     def should_element_disapper(self):
         assert self.is_disappeared(*ProductPageLocators.DISAPPERED_ELEMENT), \
             "Success message is not dissapeared, but should be"
 
-    def add_to_basket(self):
-        self.click_add_to_basket_button()
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
